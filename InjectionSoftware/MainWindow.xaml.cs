c5be035247a7f1351361b5f03f065cad3d76d2ed
Ng Kelvin <yuhinng@hotmail.com>
2020-07-14 19:22:51 +0800

    Initial Upload

---
+++ InjectionSoftware/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InjectionSoftware
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private InjectionPage injectionPage = new InjectionPage();
+        private RoomPage roomPage = new RoomPage();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //set the default selection to 1
+            leftControlBar.SelectedIndex = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hellowrold");
+        }
+
+        // switching pages
+        private void leftControlBar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListView lv = sender as ListView;
+            switch (lv.SelectedIndex)
+            {
+                case 0:
+                    Main.Content = injectionPage;
+                    break;
+                case 1:
+                    Main.Content = roomPage;
+                    break;
+                case 2:
+
+                    break;
+
+            }            
+        }
+    }
+}
