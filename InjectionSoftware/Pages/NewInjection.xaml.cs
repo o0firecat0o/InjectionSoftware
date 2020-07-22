8329a003d01f8c7a12ad8e42d0e12c7158534916
o0firecat0o <noobpancake@hotmail.com>
2020-07-22 15:26:23 +0800

    uploaded

--- InjectionSoftware/Pages/NewInjection.xaml.cs
+++ InjectionSoftware/Pages/NewInjection.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InjectionSoftware.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace InjectionSoftware.Pages
         public NewInjection()
         {
             InitializeComponent();
+            DataContext = new InjectionViewModel();
         }
     }
 }
---
+++ InjectionSoftware/ViewModels/NewInjectionViewModel.cs
@@ -0,0 +1,24 @@
+﻿using InjectionSoftware.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjectionSoftware.ViewModels
+{
+    public class NewInjectionViewModel
+    {
+        public Command Cancel { get; set; }
+
+        public NewInjectionViewModel()
+        {
+
+        }
+
+        private void cancel()
+        {
+
+        }
+    }
+}
