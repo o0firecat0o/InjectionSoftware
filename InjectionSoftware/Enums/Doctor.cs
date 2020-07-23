764ddc72f97f9c689a04e022ed9556d672ce6f41
o0firecat0o <noobpancake@hotmail.com>
2020-07-23 11:28:18 +0800

    Added enum for doctor and rp

---
+++ InjectionSoftware/Enums/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjectionSoftware.Enums
+{
+    //TODO: load doctor list from txt, instead of manual assining
+
+    public enum Doctor
+    {
+        Dr_KevinTse,
+        Dr_DonaldTse,
+        Dr_Shu,
+        Dr_Fan,
+        Dr_Cheng,
+        Dr_Antonio,
+        Dr_Siu,
+        Dr_Lo
+    }
+}
---
+++ InjectionSoftware/Enums/RP.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjectionSoftware.Enums
+{
+    [Flags]
+    public enum RP
+    {
+        Null = 0x00,
+        F18_FDG = 0x01,
+        F18_PSMA = 0x02,
+        F18_FLDopa = 0x04,
+        C11_Acetate = 0x08,
+        C11_PIB = 0x10,
+        C11_Methionine = 0x20,
+        Ga68_Dotatate = 0x40,
+        Ga68_PSMA = 0x80,
+    }
+}
