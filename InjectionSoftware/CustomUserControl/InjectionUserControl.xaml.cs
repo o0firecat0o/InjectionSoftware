5be17dd5c6e91089d7f2c9d2ed454e8fa20d5f3b
Ng Kelvin <yuhinng@hotmail.com>
2020-08-17 21:51:39 +0800

    Successfully added binding to injection user control

---
+++ InjectionSoftware/CustomUserControl/InjectionUserControl.xaml.cs
@@ -0,0 +1,58 @@
+﻿using InjectionSoftware.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InjectionSoftware.CustomUserControl
+{
+    /// <summary>
+    /// Interaction logic for InjectionUserControl.xaml
+    /// </summary>
+    public partial class InjectionUserControl : UserControl
+    {
+
+
+
+       
+
+
+
+        public InjectionUserControl()
+        {
+            InitializeComponent();
+
+        }
+
+        public static readonly DependencyProperty InjectionProperty =
+          DependencyProperty.Register("Injection", typeof(Injection), typeof(InjectionUserControl), new FrameworkPropertyMetadata()
+          {
+              PropertyChangedCallback = OnInjectionChanged,
+              BindsTwoWayByDefault = true
+          });
+
+        public Injection Injection
+        {
+            get { return (Injection)GetValue(InjectionProperty); }
+            set { SetValue(InjectionProperty, value); }
+        }
+
+        private static void OnInjectionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.OldValue != e.NewValue)
+            {
+                // code to be executed on value update
+            }
+        }
+    }
+}
--- InjectionSoftware/ViewModels/RoomPageViewModel.cs
+++ InjectionSoftware/ViewModels/RoomPageViewModel.cs
@@ -1,14 +1,16 @@
-﻿using InjectionSoftware.Enums;
+﻿using InjectionSoftware.Class;
+using InjectionSoftware.Enums;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace InjectionSoftware.ViewModels
 {
-    internal class RoomPageViewModel
+    internal class RoomPageViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<Room> AllRoom
         {
@@ -17,5 +19,37 @@ namespace InjectionSoftware.ViewModels
                 return Room.Rooms;
             }
         }
+
+        public ObservableCollection<Injection> AllInjection
+        {
+            get
+            {
+                return InjectionsManager.injections;
+            }
+        }
+
+        public RoomPageViewModel()
+        {
+            Number = 4;
+        }
+
+        private int _Number;
+        public int Number
+        {
+            get { return _Number; }
+            set { _Number = value; OnPropertyChanged("Number"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
     }
 }
