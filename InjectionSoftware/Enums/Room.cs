1ad285e80d94d0649cd34c296a467170d20a7805
o0firecat0o <noobpancake@hotmail.com>
2020-09-15 16:16:07 +0800

    Added multiplepatientallowed in room.cs

--- InjectionSoftware/Enums/Room.cs
+++ InjectionSoftware/Enums/Room.cs
@@ -15,6 +15,8 @@ namespace InjectionSoftware.Enums
         public string Name { get; }
         public Brush Color { get; }
 
+        public bool MultiplePatientAllowed { get; set; }
+
         public float RowSpan { get; set; }
         public float ColumnSpan { get; set; }
 
@@ -22,7 +24,7 @@ namespace InjectionSoftware.Enums
         /// Used for grid inside grid, the larger the cell, the more child it could handle, for ex, room 11
         /// </summary>
         public int NumberOfChildColumn { get { return (int)(ColumnSpan / 5); } }
-        public int NumberOfChildRow { get { return Math.Max((int)(RowSpan / 3), (Injections.Count + NumberOfChildColumn - 1) / NumberOfChildColumn ); } }
+        public int NumberOfChildRow { get { return Math.Max((int)(RowSpan / 3), (Injections.Count + NumberOfChildColumn - 1) / NumberOfChildColumn); } }
 
         public float Row { get; set; }
         public float Column { get; set; }
@@ -56,7 +58,7 @@ namespace InjectionSoftware.Enums
             return Rooms[0];
         }
 
-        public Room(string Name, Brush Color, float RowSpan, float ColumnSpan, float Row, float Column)
+        public Room(string Name, Brush Color, float RowSpan, float ColumnSpan, float Row, float Column, bool MultiplePatientAllowed = false)
         {
             this.Name = Name;
             this.Color = Color;
@@ -64,6 +66,7 @@ namespace InjectionSoftware.Enums
             this.ColumnSpan = ColumnSpan;
             this.Row = Row;
             this.Column = Column;
+            this.MultiplePatientAllowed = MultiplePatientAllowed;
             Rooms.Add(this);
         }
 
@@ -71,13 +74,13 @@ namespace InjectionSoftware.Enums
         {
             new Room("NM", (Brush)converter.ConvertFromString("#FFE4C4"), 3, 5, 0, 0);
             new Room("Treadmill", (Brush)converter.ConvertFromString("#C4F9FF"), 3, 5, 3, 0);
-            new Room("Changing\nRooms", (Brush)converter.ConvertFromString("#C4F9FF"), 9, 5, 6, 0);
-            new Room("Lobby", (Brush)converter.ConvertFromString("#F0C4FF"), 3, 10, 12, 5);
+            new Room("Changing\nRooms", (Brush)converter.ConvertFromString("#C4F9FF"), 9, 5, 6, 0, true);
+            new Room("Lobby", (Brush)converter.ConvertFromString("#F0C4FF"), 3, 10, 12, 5, true);
             new Room("MCT", (Brush)converter.ConvertFromString("#FFE4C4"), 3, 5, 0, 5);
-            new Room("7", (Brush)converter.ConvertFromString("#C4FFCE"), 5, 13, 0, 10);
+            new Room("7", (Brush)converter.ConvertFromString("#C4FFCE"), 5, 13, 0, 10, true);
             new Room("BIO", (Brush)converter.ConvertFromString("#FFE4C4"), 3, 10, 0, 23);
 
-            new Room("8", (Brush)converter.ConvertFromString("#C4FFCE"), 3, 10, 3, 23);
+            new Room("8", (Brush)converter.ConvertFromString("#C4FFCE"), 3, 10, 3, 23, true);
 
             new Room("9", (Brush)converter.ConvertFromString("#C4FFCE"), 3, 5, 6, 23);
             new Room("3", (Brush)converter.ConvertFromString("#C4FFCE"), 3, 5, 9, 23);
@@ -87,7 +90,7 @@ namespace InjectionSoftware.Enums
             new Room("4", (Brush)converter.ConvertFromString("#C4FFCE"), 3, 5, 9, 28);
             new Room("5", (Brush)converter.ConvertFromString("#C4FFCE"), 3, 5, 12, 28);
 
-            new Room("11", (Brush)converter.ConvertFromString("#C4FFCE"), 6, 10, 6, 6);
+            new Room("11", (Brush)converter.ConvertFromString("#C4FFCE"), 6, 10, 6, 6, true);
             new Room("10", (Brush)converter.ConvertFromString("#C4FFCE"), 3, 5, 9, 16);
             new Room("2", (Brush)converter.ConvertFromString("#C4FFCE"), 3, 5, 6, 16);
             new Room("PetMR", (Brush)converter.ConvertFromString("#FFE4C4"), 3, 8, 12, 15);
